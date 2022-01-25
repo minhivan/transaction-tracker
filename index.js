7e352168c8a979284a0769491f5aa0641463d2dd
minh.pn <minh.pn811@gmail.com>
2022-01-25 14:29:08 +0700

    Update test case

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const elasticService = require("./services/elastic.service")
+let getData = require('./controllers/tracker.controller');
 
-// init recursive call
+
+
+(async function () {
+    await getData();
+})()
+
+
+// (async function () {
+//     for await (let val of process.argv) {
+//         switch (val) {
+//             case "sync_coin_detail": // Loop coin list
+//                 await getData();
+//                 //await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
+//                 break;
+//         }
+//     }
+// })()
