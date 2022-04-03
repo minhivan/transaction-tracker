12adf862bf22dc0f0fca5a8f9657022888384a76
minh.pn <minh.pn811@gmail.com>
2022-04-04 01:00:02 +0700

    [update] update index worker

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-let getData = require('./controllers/tracker.controller');
+let {getData, getSmartContractData} = require('./controllers/tracker.controller');
 
-
-
-(async function () {
-    await getData();
-})()
 
 
 // (async function () {
-//     for await (let val of process.argv) {
-//         switch (val) {
-//             case "sync_coin_detail": // Loop coin list
-//                 await getData();
-//                 //await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
-//                 break;
-//         }
-//     }
+//     await getData();
 // })()
+
+
+(async function () {
+    for await (let val of process.argv) {
+        switch (val) {
+            case "transaction": // Loop coin list
+                await getData();
+                //await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
+                break;
+            default:
+                await getSmartContractData();
+        }
+    }
+})()
