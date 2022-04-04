df455f539f9173331f9b335619e1b20d26dd39be
minh.pn <minh.pn811@gmail.com>
2022-04-04 14:39:52 +0700

    [update] update test

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
-// const Web3EthAbi = require('web3-eth-abi');
+const Web3EthAbi = require('web3-eth-abi');
+let rs = Web3EthAbi.encodeFunctionSignature('claim(uint64)');
+
+let input = "0xaab8ab0c0000000000000000000000000000000000000000000000000000000000000000";
+if (input.includes(rs)) {
+    console.log("Is claim")
+}
+// const InputDataDecoder = require('ethereum-input-data-decoder');
+// const decoder = new InputDataDecoder(`${__dirname}/config/abi/zomland.json`);
 //
-// let rs = Web3EthAbi.encodeFundectionSignature('claim(uint64)');
-// console.log(rs)
-
-const InputDataDecoder = require('ethereum-input-data-decoder');
-const decoder = new InputDataDecoder(`${__dirname}/config/abi/zomland.json`);
-
-
-const data = `0xaab8ab0c000000000000000000000000000000000000000000000000000000000000001f`;
-
-const result = decoder.decodeData(data);
-
-console.log(result);
+//
+// const data = `0xaab8ab0c000000000000000000000000000000000000000000000000000000000000001f`;
+//
+// const result = decoder.decodeData(data);
+//
+// console.log(result);
