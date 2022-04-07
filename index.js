7d55e559cdd36e9e9fa8c6b2b8d254a9cf2a2bac
minh.pn <minh.pn811@gmail.com>
2022-04-07 14:49:00 +0700

    [update] update app js process argv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let {getData, getSmartContractData} = require('./controllers/tracker.controller'
 (async function () {
     for await (let val of process.argv) {
         switch (val) {
-            case "transaction":
+            case "wallet":
                 await getData();
                 break;
             case "smart_contract":
