637d83c281b016a27e8730b62fbafa0d2080fc8a
minh.pn <minh.pn811@gmail.com>
2022-04-07 14:41:09 +0700

    [update] update controller tracker

--- controllers/tracker.controller.js
+++ controllers/tracker.controller.js
@@ -7,15 +7,20 @@ const Web3EthAbi = require('web3-eth-abi');
 
 let currentBlockWallet = 0;
 let currentBlockContract = 0;
-let pathPrefixSmartContract = `/api?module=account&action=txlist&address=${process.env.WALLET_MASTER}&startblock=${currentBlockWallet}&sort=asc&apikey=${process.env.BSCSCAN_API_KEY}`;
-let pathPrefixWallet = `/api?module=account&action=txlist&address=${process.env.WALLET_TO}&startblock=${currentBlockContract}&sort=asc&apikey=${process.env.BSCSCAN_API_KEY}`;
+let count_process_1 = 0;
+let count_process_2 = 0;
 
 // init recursive call
 const getData = async() => {
+    if (count_process_1 > 1000)
+        process.exit(1);
+
     await new Promise(resolve => setTimeout(resolve, process.env.BLOCK_PER_SECOND * 1000))
     try {
-        const path = process.env.BSCSCAN_URL + pathPrefixWallet;
-        let response = await  axios.get(path);
+        console.log(" [*] Starting tracker data from wallet");
+        let path = `/api?module=account&action=txlist&address=${process.env.WALLET_TO}&startblock=${currentBlockContract}&sort=asc&apikey=${process.env.BSCSCAN_API_KEY}`;
+        const url = process.env.BSCSCAN_URL + path;
+        let response = await  axios.get(url);
         let data = await response.data;
 
         if (!isArray(data.result)){
@@ -28,12 +33,15 @@ const getData = async() => {
             throw new Error("Wrong records");
         }
 
-        if (parseInt(latestRecords.blockNumber) !== currentBlockWallet) {
-            currentBlockWallet = parseInt(latestRecords.blockNumber);
+
+        if (Number(latestRecords.blockNumber) !== Number(currentBlockWallet)) {
+            currentBlockWallet = Number(latestRecords.blockNumber);
             // import
-            console.log(" [x] Import data wallet tracker")
+            console.log(" [x] Starting import data to elasticsearch");
             elasticService.create_bulk('transaction_tracker', data.result)
         }
+        console.log(` [x] Done process tracker - ${count_process_1} times`);
+        count_process_1 ++;
         await getData(); // recursive
     } catch (e) {
         console.log(e.message)
@@ -46,13 +54,16 @@ const getData = async() => {
 
 // init recursive call
 const getSmartContractData = async() => {
-    // if (count > 10)
-    //     process.exit(1);
-    await new Promise(resolve => setTimeout(resolve, process.env.BLOCK_PER_SECOND * 1000));
+    if (count_process_2 > 1000)
+        process.exit(1);
 
+    await new Promise(resolve => setTimeout(resolve, process.env.BLOCK_PER_SECOND * 1000));
     try {
-        const path = process.env.BSCSCAN_URL + pathPrefixSmartContract;
-        let response = await  axios.get(path);
+        console.log(" [*] Starting tracker data from smart contract");
+        let path = `/api?module=account&action=txlist&address=${process.env.WALLET_MASTER}&startblock=${currentBlockWallet}&sort=asc&apikey=${process.env.BSCSCAN_API_KEY}`;
+
+        const url = process.env.BSCSCAN_URL + path;
+        let response = await axios.get(url);
         let data = await response.data;
 
         if (!isArray(data.result)){
@@ -65,17 +76,19 @@ const getSmartContractData = async() => {
             throw new Error("Wrong records");
         }
 
-        if (parseInt(latestRecords.blockNumber) !== currentBlockContract) {
-            currentBlockContract = parseInt(latestRecords.blockNumber);
+        if (Number(latestRecords.blockNumber) !== Number(currentBlockContract)) {
+            currentBlockContract = Number(latestRecords.blockNumber);
+            let methodHash = Web3EthAbi.encodeFunctionSignature('claim(uint64)');
             let filtered = data.result.filter(function(value, index, arr) {
-                let methodHash = Web3EthAbi.encodeFunctionSignature('claim(uint64)');
                 return value.input.includes(methodHash);
             })
-            // import
-            console.log(" [x] Import data smart contract tracker")
+            // import data
+            console.log(" [x] Starting import data to elasticsearch");
             elasticService.create_bulk('transaction_smart_contract_tracker', data.result);
             elasticService.create_bulk('transaction_smart_contract_claim_tracker', filtered);
         }
+        console.log(` [x] Done process tracker - ${count_process_2} times`);
+        count_process_2 ++;
         // console.timeEnd("answer time");
         await getSmartContractData();
     } catch (e) {
